18436651d4c232d78e5172b208ce01116424ac6b
anbojjav <anbojjav@192.168.1.13>
2018-03-05 20:42:03 -0800

    kth smallest element

---
+++ interviews/datastructures-algorithms/amzn2/KthSmallestElement.java
@@ -0,0 +1,110 @@
+/*
+ * Given an unsorted array, find kth minimum element.
+ * 
+ * TC: O(nlogk)
+ * SC: O(logk)
+ */
+package amzn2;
+
+import java.util.Arrays;
+
+public class KthSmallestElement {
+	public int findKthSmallestElement(int[] a, int k) throws Exception
+	{
+		if(a==null || a.length<1 || k>a.length)
+			throw new IllegalArgumentException();
+		
+		int n = a.length, heapSize;
+		int[] maxHeap = new int[k+1];
+		maxHeap[0] = 0;
+		
+		// build a heap of size k
+		for(int i=0; i<k; i++)
+		{
+			maxHeap[i+1] = a[i];
+		}
+		buildMaxHeap(maxHeap, k+1);
+		//System.out.println("initial heap: " + Arrays.toString(maxHeap));
+		
+		// maxHeapify in each iteration
+		for(int i=k; i<n; i++)
+		{
+			if(maxHeap[1]>a[i])
+			{
+				maxHeap[1] = a[i];
+				maxHeapify(maxHeap, k+1, 1);
+			}
+		}
+		
+		// return min element
+		return maxHeap[1];
+	}
+	
+	// called only once, when building the heap for the first time
+	public int[] buildMaxHeap(int[] heap, int heapSize)
+	{
+		if(heap == null || heap.length < 1)
+			return null;
+		
+		int lastElementIndex = heap.length-1;
+		int parentIndex = lastElementIndex/2; // this gives the parent of lastElementIndex
+		
+		for(int i=parentIndex; i>0; i--)
+		{
+			maxHeapify(heap, heapSize, i);
+		}
+		
+		return heap;
+	}
+	
+	public int[] maxHeapify(int[] heap, int heapSize, int i)
+	{
+		if(heap == null || heap.length < 1)
+			return null;
+		
+		int l = 2*i;
+		int r = 2*i+1;
+		int largest = i;
+		
+		if(l < heapSize && heap[l] > heap[largest])
+			largest = l;
+		if(r < heapSize && heap[r] > heap[largest])
+			largest = r;
+				
+		if(i != largest)
+		{
+			int temp = heap[i];
+			heap[i] = heap[largest];
+			heap[largest] = temp;
+			
+			maxHeapify(heap, heapSize, largest);
+			
+		}
+		
+		return heap;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		KthSmallestElement ke = new KthSmallestElement();
+		int k;
+		
+		int[] test1 = {-2, -1, -6}; 
+		k = 3;
+		System.out.println(k + "rd smallest elemnt is " + ke.findKthSmallestElement(test1, k));
+		Arrays.sort(test1);;
+		System.out.println(Arrays.toString(test1) + "\n"); // -1
+		
+		int[] test2 = {2, 1, 9, 8, 7, 6, 3, 5, 4};
+		k = 5;
+		System.out.println(k + "th smallest elemnt is " + ke.findKthSmallestElement(test2, k));
+		Arrays.sort(test2);;
+		System.out.println(Arrays.toString(test2) + "\n"); // 5
+		
+		int[] test3 = {-2, -11, 0, -9, 8, 7, -6, 3, 5, 4};
+		k = 8;
+		System.out.println(k + "th smallest elemnt is " + ke.findKthSmallestElement(test3, k));
+		Arrays.sort(test3);;
+		System.out.println(Arrays.toString(test3) + "\n"); // 5
+	}
+}
--- interviews/datastructures-algorithms/amzn2/MinDifferencePair.java
+++ interviews/datastructures-algorithms/amzn2/MinDifferencePair.java
@@ -1,5 +1,58 @@
+/*
+ * Given 2 array of size m , n , find the pair from diff array with minimum difference .
+ * 
+ * TC: O(nlogn)
+ */
+
 package amzn2;
 
+import java.util.Arrays;
+
 public class MinDifferencePair {
+	public static int[] findPair(int[] a, int[] b)
+	{
+		if(a==null || b==null)
+			return null;
+		
+		// merge sort arrays, O(nlogn)
+		Arrays.sort(a);
+		Arrays.sort(b);
+		
+		int[] minPair = new int[2];
+		int m = a.length, n = b.length;
+		int i = 0, j = 0;
+		int min = Integer.MAX_VALUE, currMin;
+		
+		// merging as in merge sort, O(logn)
+		while(i<m && j<n)
+		{
+			currMin = Math.abs(a[i]-b[j]);
+			
+			if(currMin < min)
+			{
+				min = currMin;
+				minPair[0] = a[i];
+				minPair[1] = b[j];
+			}
+			
+			if(min == 0)
+				break;
+			
+			if(a[i]<b[j])
+				i++;
+			else
+				j++;
+		}
+		
+		return minPair;
+	}
 	
+	public static void main(String[] args)
+	{
+		int[] a = {2, 5, 11};
+		int[] b = {10, 9, 4, 1};
+		
+		int[] res = findPair(a, b);
+		System.out.println(Arrays.toString(res));
+	}
 }
