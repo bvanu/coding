493a90acb44c92c333450489a9224e71fe3a2ad4
wanheda <4624885+bvanusha@users.noreply.github.com>
2019-12-03 06:23:30 -0800

    Update NextPermutation.java

--- interviews/datastructures-algorithms/amzn2/NextPermutation.java
+++ interviews/datastructures-algorithms/amzn2/NextPermutation.java
@@ -5,21 +5,145 @@ If such arrangement is not possible, it must rearrange it as the lowest possible
 
 The replacement must be in-place and use only constant extra memory.
 
- * Here’s my attempt to simplify the intuition:
+ * Single Pass Approach
 
-Let’s say we define
-number refers to the value of the whole array. In the example provided, since we have 6, 3, 4, 9, 8, 7, 1, so the value of number is 6349871.
-element[i] refers to specific value of an element in the array. Here element[2] is 4.
+Algorithm
 
-First we should note two important properties:
+First, we observe that for any given sequence that is in descending order, no next larger permutation is possible. For example, no next permutation is possible for the following array:
 
-Number is the smallest one when all the elements all in ascending order, no matter how the permutation could perform on it.
-Number is the greatest one when all the elements all in descending order, no matter how the permutation could perform on it.
+[9, 5, 4, 3, 1]
+We need to find the first pair of two successive numbers 
+a
+[
+i
+]
+a[i] and 
+a
+[
+i
+−
+1
+]
+a[i−1], from the right, which satisfy 
+a
+[
+i
+]
+>
+a
+[
+i
+−
+1
+]
+a[i]>a[i−1]. Now, no rearrangements to the right of 
+a
+[
+i
+−
+1
+]
+a[i−1] can create a larger permutation since that subarray consists of numbers in descending order. Thus, we need to rearrange the numbers to the right of 
+a
+[
+i
+−
+1
+]
+a[i−1] including itself.
 
-The goal is to find the next smallest number that is greater than its current number. So we first find the first two adjacent ascending elements from the right, which are 4 & 9 in this example, respectively. We don’t need to do anything with the elements that are in front of these two.
+Now, what kind of rearrangement will produce the next larger number? We want to create the permutation just larger than the current one. Therefore, we need to replace the number 
+a
+[
+i
+−
+1
+]
+a[i−1] with the number which is just larger than itself among the numbers lying to its right section, say 
+a
+[
+j
+]
+a[j].
 
-Then because we want the whole number to be the smallest number that is greater than its current number, we want 4 to be replaced with the smallest number from the later array that is greater than 4, which is 7 here
+ Next Permutation 
 
+We swap the numbers 
+a
+[
+i
+−
+1
+]
+a[i−1] and 
+a
+[
+j
+]
+a[j]. We now have the correct number at index 
+i
+−
+1
+i−1. But still the current permutation isn't the permutation that we are looking for. We need the smallest permutation that can be formed by using the numbers only to the right of 
+a
+[
+i
+−
+1
+]
+a[i−1]. Therefore, we need to place those numbers in ascending order to get their smallest permutation.
+
+But, recall that while scanning the numbers from the right, we simply kept decrementing the index until we found the pair 
+a
+[
+i
+]
+a[i] and 
+a
+[
+i
+−
+1
+]
+a[i−1] where, 
+a
+[
+i
+]
+>
+a
+[
+i
+−
+1
+]
+a[i]>a[i−1]. Thus, all numbers to the right of 
+a
+[
+i
+−
+1
+]
+a[i−1] were already sorted in descending order. Furthermore, swapping 
+a
+[
+i
+−
+1
+]
+a[i−1] and 
+a
+[
+j
+]
+a[j] didn't change that order. Therefore, we simply need to reverse the numbers following 
+a
+[
+i
+−
+1
+]
+a[i−1] to get the next smallest lexicographic permutation.
 change
 6, 3, 4, 9, 8, 7, 1
 to
