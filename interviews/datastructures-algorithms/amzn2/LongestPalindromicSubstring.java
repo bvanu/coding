2e85cb35c91248c175d492c8cdb7d4d94c70954b
wanheda <4624885+bvanusha@users.noreply.github.com>
2019-12-16 07:29:38 -0800

    Update LongestPalindromicSubstring.java

--- interviews/datastructures-algorithms/amzn2/LongestPalindromicSubstring.java
+++ interviews/datastructures-algorithms/amzn2/LongestPalindromicSubstring.java
@@ -8,47 +8,57 @@ Example 2:
 Input: "cbbd"
 Output: "bb"
 
-TC: O(n^2)
-SC: O(1)
+Approach 4: Expand Around Center
+
+In fact, we could solve it in O(n^2) time using only constant space.
+
+We observe that a palindrome mirrors around its center. Therefore, a palindrome can be expanded from its center, and there are 
+only 2n−1 such centers.
+
+You might be asking why there are 2n−1 but not n centers? The reason is the center of a palindrome can be in between two 
+letters. Such palindromes have even number of letters (such as "abba") and its center are between the two 'b's.
+
+Complexity Analysis
+Time complexity : O(n^2). Since expanding a palindrome around its center could take O(n) time, the overall complexity is O(n^2).
+Space complexity : O(1). 
+
  */
 package amzn2;
 
 public class LongestPalindromicSubstring {
-	int lo;
-    int maxLength;
-        
-    public LongestPalindromicSubstring()
-    {
-         lo = 0;
-         maxLength = 0;
-    }
-    
     public String longestPalindrome(String s) {
-        if(s==null ||s.length()<1)
+        int n = s.length();
+        
+        if(s==null || n<=1)
             return s;
         
-        for(int i=0; i<s.length(); i++)
-        {
-            extendPalindrome(s, i, i);
-            extendPalindrome(s, i, i+1);
+        int start = 0, end = 0;
+        
+        for(int i=0; i<n; i++)
+        { 
+            int len1 = extendPalindrome(s,i, i);
+            int len2 = extendPalindrome(s, i, i+1);
+            
+            int len = Math.max(len1, len2);
+            
+            if(len>end-start+1)
+            {
+                start = i-(len-1)/2;
+                end = i+len/2;
+            }
         }
         
-        return s.substring(lo, lo+maxLength);
+        return s.substring(start, end+1);
     }
     
-    public void extendPalindrome(String s, int start, int end)
+    private int extendPalindrome(String s, int left, int right)
     {
-        while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end))
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right))
         {
-            start--;
-            end++;
+            left--;
+            right++;
         }
         
-        if(maxLength<=end-start-1)
-        {
-            lo = start+1;
-            maxLength = end-start-1;
-        }
-            
+        return right-left-1; // (right-1) + (left+1) -1
     }
 }
